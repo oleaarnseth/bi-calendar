3ecb687f4e8bdfbe5ec1bbdf7ea64abcc253f843
Ole Aarnseth <oaarnseth@solverglobal.com>
2025-08-24 19:53:30 +0200

    Add swagger doc

--- BICalendar/Program.cs
+++ BICalendar/Program.cs
@@ -9,9 +9,22 @@ builder.Services.AddHttpClient<IRequestService, BIRequestService>();
 builder.Services.AddScoped<ICalendarEventService, BICalendarEventService>();
 builder.Services.AddMemoryCache();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+//app.MapGet("/", () => "Hello World!");
 
 //app.MapGet("/runtime", async () =>
 //{
@@ -31,6 +44,12 @@ app.MapPost("/api/bi-calendar-events", async (CalendarEventsQuery calendarEvents
 
     // Return result
     return Results.Ok(result);
-});
+})
+.WithName("GetBICalendarEvents") // operationId in Swagger
+.WithTags("Calendar Events")   // groups endpoints in Swagger
+.Accepts<CalendarEventsQuery>("application/json")
+.Produces<IEnumerable<CalendarEvent>>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status400BadRequest)
+.WithOpenApi();
 
 app.Run();
